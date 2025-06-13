0a651ea5a5ae2879fb28a08cf61dd63e82981354
kkkkl-dev <moutazjehadodeh0808@gmail.com>
2025-06-14 01:40:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["1226206631801655376", ""], 
   mongodbUri : "",
   spotifyClientId : "",
   spotifyClientSecret : "",
